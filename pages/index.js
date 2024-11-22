c615c843f03a432f1c3f4aa7bc8689b2460b30ea
Luna Schenk <schenk.luna@gmail.com>
2024-11-22 16:09:07 +0100

    add sepolia scan API on frontpage

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Layout from "../components/Layout";
 import WaveText from "../components/WaveText";
 import BlockGrid from "../components/BlockGrid";
+import LatestTransactions from "../components/LatestTransactions";
 
 class Home extends Component {
     render() {
@@ -10,6 +11,9 @@ class Home extends Component {
             <WaveText text="GIMME MONEY" />
             <h3 style={{ textAlign: "center" }}>Crowdfunding on the blockchain.</h3>
             <BlockGrid />
+            <div style={{ marginTop: "40px" }}>
+            <LatestTransactions />
+            </div>
         </Layout>
         );
     }
